2f771f0deaed8a7b09024199fd81a1a88118a2bf
Mohit Maulekhi <mohitmaulekhi312@gmail.com>
2024-08-27 13:10:50 +0530

    initial commit with all libraries setup completed

--- app/index.js
+++ app/index.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { SafeAreaView } from 'react-native-safe-area-context';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Image source = {require("../assets/images.jpeg")}/>
+    </SafeAreaView>
   );
 }
 
